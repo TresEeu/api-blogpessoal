2ed3ed602ed204cf9047002bbaf26305369af1db
Raphaelno <raphaelporque@gmail.com>
2022-11-04 12:11:21 -0300

    Parte 1: Postagem

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
+      entities: [],
+      synchronize: true,
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
