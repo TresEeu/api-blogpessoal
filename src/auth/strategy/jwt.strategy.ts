58d35e06cbe6bb261f895d8f1edc90146bbd94a7
Raphael <112710346+TresEeu@users.noreply.github.com>
2022-12-06 12:46:16 -0300

    Update jwt.strategy.ts

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtConstants } from '../constants/constants';
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { jwtConstants } from "../constants/constants";
 
 @Injectable()
-export class JwStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(){
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: jwtConstants.secret,
-        });
+            secretOrKey: jwtConstants.secret
+        })
     }
 
     async validate(payload: any) {
-        return { userId: payload.sub, usarname: payload.username };
+        return {userId: payload.sub, username: payload.username}
     }
 }
